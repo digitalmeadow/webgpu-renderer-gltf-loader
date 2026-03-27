0430d36908acd438d1615323679f12377b3df317
Laird Kruger <accounts@lairdkruger.com>
2026-03-28 06:54:31 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: "ControlPanel",
+      name: "WebGPU Renderer GLTF Loader",
       fileName: "index",
     },
   },
